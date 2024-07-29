1e14a036c695c5b31a994584f0f0960c0fedbbea
acemi159 <61695430+acemi159@users.noreply.github.com>
2024-07-29 16:21:43 +0200

    Basic PDF template with invoice nr

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 import glob
-
+from pathlib import Path
 import pandas as pd
+from fpdf import FPDF
 
 
 filepaths = glob.glob("samples/*.xlsx")
 
 for filepath in filepaths:
     df = pd.read_excel(filepath, sheet_name="Sheet 1")
-    print(df)
+    
+    filename = Path(filepath).stem
+    invoice_nr = filename.split("-")[0]
+    
+    # Initialize the PDF file
+    pdf = FPDF(orientation="P", unit="mm", format="A4")
+    pdf.add_page()
+    
+    # Implement the template to fill with data.
+    pdf.set_font(family="Times", size=16, style="B")
+    pdf.cell(w=50, h=8, txt=f"Invoice nr. {invoice_nr}")
+    
+    # Save the output PDF file.
+    pdf.output(f"PDFs/{filename}.pdf")
